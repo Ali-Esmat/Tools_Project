5c1a3cb88c26a6306f2cd28c7c8c23e25108feaf
Ali Esmat <aliesmat612@gmail.com>
2023-12-31 00:25:49 +0200

    Updated script.sh to expose backend

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ docker build -t frontend ./frontend/
 docker network create mynetwork
 docker run --name database --network=mynetwork -d database:latest
 sleep 5
-docker run --name backend --env-file ./Backend/.env --network=mynetwork -d backend:latest
+docker run --name backend --env-file ./Backend/.env -p 8000:8000 --network=mynetwork -d backend:latest
 docker run --name frontend --env-file ./frontend/.env -p 3000:3000 --network=mynetwork -d frontend:latest
