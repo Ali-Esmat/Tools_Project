046a8d9fea0fe999f763844a0ac5601f33d6e15d
Ali Esmat <aliesmat612@gmail.com>
2023-12-30 21:24:29 +0200

    Reused env files

---
+++ script.sh
@@ -0,0 +1,8 @@
+docker build -t database .
+docker build -t backend ./Backend/
+docker build -t frontend ./frontend/
+docker network create mynetwork
+docker run --name database --network=mynetwork -d database:latest
+sleep 5
+docker run --name backend --env-file ./Backend/.env --network=mynetwork -d backend:latest
+docker run --name frontend --env-file ./frontend/.env -p 3000:3000 --network=mynetwork -d frontend:latest
